484b658e8681bad09d4d842e4128e405b2e81509
kyonezu <yonezu.dev@gmail.com>
2018-12-12 15:19:30 +0900

    新しいJSON形式対応

--- index.js
+++ index.js
@@ -1,132 +1,134 @@
 function main() {
-	const word = getWord();
-	console.log(word);
-	getWords(word)
-		.then((words) => createView(words))
-		.then((view) => displayView(view))
-		.catch((error) => {
-			console.error(`エラーが発生しました (${error})`);
-		});
+  const word = getWord();
+  getWords(word)
+    .then((data) => createView(data))
+    .then((view) => displayView(view))
+    .catch((error) => {
+      console.error(`エラーが発生しました (${error})`);
+    });
 }
 
 function getWords(word) {
-	return new Promise((resolve, reject) => {
-		const request = new XMLHttpRequest();
-		var url = word ?
-			`https://youtube-title-mecab.herokuapp.com/search/${word}` : `https://youtube-title-mecab.herokuapp.com/random`;
-		request.open("GET", url);
-			request.addEventListener("load", (event) => {
-				if (event.target.status !== 200) {
-					reject(new Error(`${event.target.status}: ${event.target.statusText}`));
-				}
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    var url = word ?
+      // `https://youtube-title-mecab.herokuapp.com/search?word=${word}` : `https://youtube-title-mecab.herokuapp.com/random`;
+      `https://youtube-title-mecab-test.herokuapp.com/search?word=${word}` : `https://youtube-title-mecab-test.herokuapp.com/random`;
+    request.open("GET", url);
+    request.addEventListener("load", (event) => {
+      if (event.target.status !== 200) {
+        reject(new Error(`${event.target.status}: ${event.target.statusText}`));
+      }
 
-				const words = JSON.parse(event.target.responseText);
-
-				console.log(words);
-
-				resolve(words);
-			});
-			request.addEventListener("error", () => {
-				reject(new Error("ネットワークエラー"));
-			});
-			request.send();
-		});
+      const data = JSON.parse(event.target.responseText);
+      console.log(data);
+      resolve(data);
+    });
+    request.addEventListener("error", () => {
+      reject(new Error("ネットワークエラー"));
+    });
+    request.send();
+  });
 }
 
 function getWord() {
-	const value = document.getElementById("word").value;
-	return encodeURIComponent(value);
+  const value = document.getElementById("word").value;
+  return encodeURIComponent(value);
 }
 
-function createView(words) {
-	let html = `<div class="balloon" style="margin-top: 30px;"><div class="container with-title"><h3 class="title">たんご</h3>`
-		+ `<p>${words[0]}, ${words[1]}, ${words[2]}, ${words[3]}</p></div>`;
+function createView(data) {
+  let words = data.words;
+  let html = `<div class="balloon" style="margin-top: 30px;"><div class="container with-title"><h3 class="title">たんご</h3>`
+    + `<p>${words[0]}, ${words[1]}, ${words[2]}, ${words[3]}</p></div>`;
 
-	html += `<div class="container with-title"><h3 class="title">たいとる</h3>`;
+  html += `<div class="container with-title"><h3 class="title">たいとる</h3>`;
 
-	// shuffleとtitle
-    var category = data.categoryId;
-    switch(category)
-    {
-        case "1": //映画とアニメ
-            words = _.shuffle(words);
-            html += `<p>${words[0]}の中心で${words[1]}への愛を叫ぶ</p>`;
-            words = _.shuffle(words);
-            html += `<p>『${words[0]}と${words[1]}』をレビュー</p>`;
-            words = _.shuffle(words);
-            html += `<p>『${words[0]}』から学んだ${words[1]}のこと</p>`;
-            words = _.shuffle(words);
-            html += `<p>【架空映画】『${words[0]}と${words[1]}』の架空予告やってみた</p>`;
-        case "2": //自動車と乗り物
-            words = _.shuffle(words);
-            html += `<p>${words[0]}の中心で${words[1]}への愛を叫ぶ</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}で${words[1]}を100倍面白くする方法</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}で1人${words[1]}</p>`;
-        case "10": //音楽、テンプレで
-            words = _.shuffle(words);
-            html += `<p>${words[0]}の中心で${words[1]}への愛を叫ぶ</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}で${words[1]}を100倍面白くする方法</p>`;
-            words = _.shuffle(words);
-            html += `<p>『${words[0]}』と『${words[1]}』を歌ってみた</p>`;
-            words = _.shuffle(words);
-            html += `<p>『${words[0]}』と『${words[1]}』を踊ってみた</p>`;
-            words = _.shuffle(words);
-            html += `<p>【声真似】『${words[0]}』と『${words[1]}』を歌ってみた</p>`;
-            words = _.shuffle(words);
-            html += `<p>『${words[0]}』と『${words[1]}』をヘリウムガス吸って歌ってみた</p>`;
-        case "15":  //ペットと動物
-            words = _.shuffle(words);
-            html += `<p>${words[0]}の中心で${words[1]}への愛を叫ぶ</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}で${words[1]}サプライズ</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}と${words[1]}で遊んでみた</p>`;
-            words = _.shuffle(words);
-            html += `<p>\[完全${words[0]}\]${words[1]}必勝法</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}使って1人${words[1]}</p>`;
-        case "17":  //スポーツ
-            html += `<p>${words[0]}の中心で${words[1]}への愛を叫ぶ</p>`;
-            words = _.shuffle(words);
-            html += `<p>\[完全勝利\]${words[1]}必勝法</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}で${words[1]}を100倍面白くする方法</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}使って1人${words[1]}</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}と${words[1]}で新しい競技を考えてみた</p>`;
-        case "19": //旅行とイベント
-            words = _.shuffle(words);
-            html += `<p>${words[0]}で${words[1]}を100倍面白くする方法</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}使って1人${words[1]}</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}の中心で${words[1]}への愛を叫ぶ</p>`;
-            words = _.shuffle(words);
-            html += `<p>【完全制覇】${words[1]}攻略法</p>`;
-            
-        default:
-            words = _.shuffle(words);
-            html += `<p>${words[0]}と${words[1]}一気飲み</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}の中心で${words[1]}への愛を叫ぶ</p>`;
-            words = _.shuffle(words);
-            html += `<p>\[完全${words[0]}\]${words[1]}必勝法</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}で${words[1]}を100倍面白くする方法</p>`;
-            words = _.shuffle(words);
-            html += `<p>${words[0]}使って1人${words[1]}</p>`;
-    }
+  // 入力した単語はタイトルに含める
+  let word = words[0];
+  words.shift()
 
-	html += `</div>`;
-	html += `</div>`;
-	return html;
+  // shuffleとtitle
+  switch(data.categoryId)
+  {
+    case "1": //映画とアニメ
+      words = _.shuffle(words);
+      html += `<p>${word}の中心で${words[1]}への愛を叫ぶ</p>`;
+      words = _.shuffle(words);
+      html += `<p>『${word}と${words[1]}』をレビュー</p>`;
+      words = _.shuffle(words);
+      html += `<p>『${word}』から学んだ${words[1]}のこと</p>`;
+      words = _.shuffle(words);
+      html += `<p>【架空映画】『${word}と${words[1]}』の架空予告やってみた</p>`;
+    case "2": //自動車と乗り物
+      words = _.shuffle(words);
+      html += `<p>${word}の中心で${words[1]}への愛を叫ぶ</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}で${words[1]}を100倍面白くする方法</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}で1人${words[1]}</p>`;
+    case "10": //音楽、テンプレで
+      words = _.shuffle(words);
+      html += `<p>${word}の中心で${words[1]}への愛を叫ぶ</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}で${words[1]}を100倍面白くする方法</p>`;
+      words = _.shuffle(words);
+      html += `<p>『${word}』と『${words[1]}』を歌ってみた</p>`;
+      words = _.shuffle(words);
+      html += `<p>『${word}』と『${words[1]}』を踊ってみた</p>`;
+      words = _.shuffle(words);
+      html += `<p>【声真似】『${word}』と『${words[1]}』を歌ってみた</p>`;
+      words = _.shuffle(words);
+      html += `<p>『${word}』と『${words[1]}』をヘリウムガス吸って歌ってみた</p>`;
+    case "15":  //ペットと動物
+      words = _.shuffle(words);
+      html += `<p>${word}の中心で${words[1]}への愛を叫ぶ</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}で${words[1]}サプライズ</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}と${words[1]}で遊んでみた</p>`;
+      words = _.shuffle(words);
+      html += `<p>\[完全${word}\]${words[1]}必勝法</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}使って1人${words[1]}</p>`;
+    case "17":  //スポーツ
+      html += `<p>${word}の中心で${words[1]}への愛を叫ぶ</p>`;
+      words = _.shuffle(words);
+      html += `<p>\[完全勝利\]${words[1]}必勝法</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}で${words[1]}を100倍面白くする方法</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}使って1人${words[1]}</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}と${words[1]}で新しい競技を考えてみた</p>`;
+    case "19": //旅行とイベント
+      words = _.shuffle(words);
+      html += `<p>${word}で${words[1]}を100倍面白くする方法</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}使って1人${words[1]}</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}の中心で${words[1]}への愛を叫ぶ</p>`;
+      words = _.shuffle(words);
+      html += `<p>【完全制覇】${words[1]}攻略法</p>`;
+
+    default:
+      words = _.shuffle(words);
+      html += `<p>${word}と${words[1]}一気飲み</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}の中心で${words[1]}への愛を叫ぶ</p>`;
+      words = _.shuffle(words);
+      html += `<p>\[完全${word}\]${words[1]}必勝法</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}で${words[1]}を100倍面白くする方法</p>`;
+      words = _.shuffle(words);
+      html += `<p>${word}使って1人${words[1]}</p>`;
+  }
+
+  html += `</div>`;
+  html += `</div>`;
+  return html;
 }
 
 function displayView(view) {
-	const result = document.getElementById("result");
-	result.innerHTML = view;
+  const result = document.getElementById("result");
+  result.innerHTML = view;
 }
