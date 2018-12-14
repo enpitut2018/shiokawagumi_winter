353677404aa639dcd9ad4179e818145b0410e3a4
PenguinGunshi <s1611396@u.tsukuba.ac.jp>
2018-12-14 16:19:51 +0900

    テンプレ分追加、URL変更

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function getWords(word) {
   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
     var url = word ?
-      // `https://youtube-title-mecab.herokuapp.com/search?word=${word}` : `https://youtube-title-mecab.herokuapp.com/random`;
-      `https:youtube-title-mecab-test.herokuapp.com/search?word=${word}` : `https://youtube-title-mecab-test.herokuapp.com/random`;
+      `https://youtube-title-mecab.herokuapp.com/search?word=${word}` : `https://youtube-title-mecab.herokuapp.com/random`;
     request.open("GET", url);
     request.addEventListener("load", (event) => {
       if (event.target.status !== 200) {
