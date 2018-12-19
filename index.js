758fd3939da4969fc8e3085437cc0366d3a0580f
kyonezu <yonezu.dev@gmail.com>
2018-12-19 14:59:59 +0900

    added video

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-function main() {
-  const word = getWord();
-
-  console.log(word);
-  document.getElementById("title").disabled = "disabled";
+function generateTitle() {
+  const word = document.getElementById("js-word").value;
+  document.getElementById("js-title-button").disabled = "disabled";
   getWords(word)
-    .then((words) => createView(words))
-
+    .then((data) => createView(data))
     .then((view) => displayView(view))
     .catch((error) => {
       console.error(`エラーが発生しました (${error})`);
+    });
+}
+
+function getMovies() {
+  const words = document.getElementsByClassName("js-words");
+  console.log(words);
+  document.getElementById("js-movie-button").style.display ="none";
+  getMovieIds(words)
+    .then((data) => createMovieView(data))
+    .then((view) => displayMovieView(view))
+    .catch((error) => {
+     console.error(`エラーが発生しました (${error})`);
     });
 }
 
@@ -34,15 +43,10 @@ function getWords(word) {
   });
 }
 
-function getWord() {
-  const value = document.getElementById("word").value;
-  return encodeURIComponent(value);
-}
-
 function createView(data) {
   let words = data.words;
   let html = `<div class="balloon" style="margin-top: 30px;"><div class="container with-title"><h3 class="title">たんご</h3>`
-    + `<p>${words[0]}, ${words[1]}, ${words[2]}, ${words[3]}</p></div>`;
+    + `<p><var class="js-words">${words[0]}</var>, <var class="js-words">${words[1]}</var>, <var class="js-words">${words[2]}</var>, <var class="js-words">${words[3]}</var></p></div>`;
 
   html += `<div class="container with-title"><h3 class="title">たいとる</h3>`;
 
@@ -133,14 +137,55 @@ function createView(data) {
         break;
   }
 
-
   html += `</div>`;
   html += `</div>`;
   return html;
 }
 
 function displayView(view) {
-  const result = document.getElementById("result");
-  result.innerHTML = view;
-  document.getElementById("title").disabled = "";
+  const title = document.getElementById("title");
+  title.innerHTML = view;
+  // 生成ボタン有効
+  document.getElementById("js-title-button").disabled = "";
+  // 動画ボタン表示
+  document.getElementById("js-movie-button").style.display ="";
 }
+
+function getMovieIds(words) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    let url =
+      `https://youtube-title-mecab-test.herokuapp.com/movie?words=${words[0].innerHTML}&words=${words[1].innerHTML}&words=${words[2].innerHTML}&words=${words[3].innerHTML}`;
+    console.log(url);
+    request.open("GET", url);
+    request.addEventListener("load", (event) => {
+      if (event.target.status !== 200) {
+        reject(new Error(`${event.target.status}: ${event.target.statusText}`));
+      }
+      const data = JSON.parse(event.target.responseText);
+      console.log(data);
+      resolve(data);
+    });
+    request.addEventListener("error", () => {
+      reject(new Error("ネットワークエラー"));
+    });
+    request.send();
+  });
+}
+
+function createMovieView(data) {
+  let movieIds = data.url;
+  let html = `<div class="balloon" style="margin-top: 30px;"><div class="container with-title"><h3 class="title">どうが</h3>`
+  for (let i = 0; i < movieIds.length; i++) {
+    html += `<iframe src="https://www.youtube.com/embed/${movieIds[i]}" frameborder="0" allowfullscreen></iframe>`
+  }
+  html += `</div>`;
+  html += `</div>`;
+  return html;
+}
+
+function displayMovieView(view) {
+  const movie = document.getElementById("movies");
+  movie.innerHTML = view;
+}
+
